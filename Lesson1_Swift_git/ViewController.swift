9488dce6b7da6f212e92ceab41ad84545f88a65c
VoldLiu <liu_heyu@126.com>
2015-09-29 20:36:29 +0800

    first commit

---
+++ Lesson1_Swift_git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson1_Swift_git
+//
+//  Created by allen on 29/9/15.
+//  Copyright (c) 2015 allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
